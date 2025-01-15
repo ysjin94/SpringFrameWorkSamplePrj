490d84828b10d3900d4d74bba0b63f34a627f9a6
SunJin_Yoon <sunjin.yoon@fnsusa.com>
2025-01-14 21:15:00 -0800

    added ThymeLeaf html

---
+++ src/main/java/hello/itemservice/basic/BasicItemController.java
@@ -0,0 +1,46 @@
+package hello.itemservice.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+//    @Autowired
+//    public BasicItemController(ItemRepository itemRepository) {
+//        this.itemRepository = itemRepository;
+//    }
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * Test Values
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("ItemA", 10, 10));
+        itemRepository.save(new Item("ItemB", 20, 20));
+    }
+
+}
+
+
